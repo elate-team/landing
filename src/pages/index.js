c1cf85d6f957761cb188fd150358d5156ec8de14
martyan <martin.juzl@gmail.com>
2019-02-17 03:16:37 +0100

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,105 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { Component } from 'react'
 import ReactFullpage from '@fullpage/react-fullpage'
+import Fade from 'react-reveal/Fade';
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-    <Layout>
-        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
+import mouseImg from '../images/mouse.png'
+import elateImg from '../images/robot.png'
+import rejdaImg from '../images/lukasrejda.png'
 
-        <ReactFullpage
-            render={({ state, fullpageApi }) => (
-                <ReactFullpage.Wrapper>
-                    <div className="section">
-                        <Link to="/page-2/">Go to page 2</Link>
-                        <button onClick={() => fullpageApi.moveSectionDown()}>
-                            Click me to move down
-                        </button>
-                    </div>
-                    <div className="section">
-                    </div>
-                    <div className="section">
-                    </div>
-                    <div className="section">
-                    </div>
-                </ReactFullpage.Wrapper>
-            )}
-        />
-    </Layout>
-)
+class IndexPage extends Component {
+
+    state = {
+        step: 1
+    }
+
+    componentDidMount = () => {
+        window.fullpage_api.destroy()
+    }
+
+    handleClick = () => {
+        this.setState({step: 2}, () => {
+            window.fullpage_api.reBuild()
+            window.fullpage_api.moveSectionDown()
+        })
+    }
+
+    render = () => {
+        const { step } = this.state
+
+        return (
+            <Layout>
+                <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
+
+                <ReactFullpage
+                    render={({ state, fullpageApi }) => (
+                        <ReactFullpage.Wrapper>
+                            <div className="section">
+                                <p className="sm">I malý</p>
+
+                                <span className="pulse"></span>
+
+                                <div className="select">
+                                    <div className="option" onClick={this.handleClick}>
+                                        <div className="label">Cíl</div>
+                                        <div className="desc">Cíl může být: nějaká meta, stav, uspořádání, objekt, kterého má být v reálném čase dosaženo</div>
+                                    </div>
+                                    <div className="option" onClick={this.handleClick}>
+                                        <div className="label">Sen</div>
+                                        <div className="desc">Sny často ukazují události, které jsou ve skutečnosti nemožné nebo nepravděpodobné</div>
+                                    </div>
+                                    <div className="option" onClick={this.handleClick}>
+                                        <div className="label">Přání</div>
+                                        <div className="desc">Něco, co byste ač už tajně či veřejně chtěli dostat nebo dát</div>
+                                    </div>
+                                </div>
+
+                                <p className="sm">Ti udělá</p>
+                                <p className="big upper">radost</p>
+                                <div className="scroll"><img src={mouseImg} /></div>
+                            </div>
+
+                            {step > 1 &&
+                                <div className="section">
+                                    <p className="sm">Na tvůj</p>
+                                    <p>whatever</p>
+                                    <p className="sm">přece nikdy není</p>
+                                    <p className="big">pozdě</p>
+
+                                    <div className="scroll"><img src={mouseImg} /></div>
+                                </div>
+                            }
+
+                            {step > 1 &&
+                                <div className="section">
+                                    <p className="sm">Jmenuju se</p>
+                                    <p className="big">Elate</p>
+                                    <img src={elateImg} />
+                                    <p className="sm">a pomůžu</p>
+                                    <p className="sm">ti tvůj <b>sen</b> splnit</p>
+
+                                    <button>Vyzkoušet</button>
+                                </div>
+                            }
+
+                            {step > 1 &&
+                                <div className="section">
+                                    <p className="sm">Na tvůj</p>
+                                    <p>whatever</p>
+                                    <p className="sm">přece nikdy není</p>
+                                    <p className="big">pozdě</p>
+                                    <img src={rejdaImg} />
+                                </div>
+                            }
+                        </ReactFullpage.Wrapper>
+                    )}
+                />
+            </Layout>
+        )
+    }
+
+}
 
 export default IndexPage
