2048387ae32da3fa05d385a624650a8d9f387a05
martyan <martin.juzl@gmail.com>
2019-02-17 01:35:59 +0100

    fullpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import ReactFullpage from '@fullpage/react-fullpage'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
+
+        <ReactFullpage
+            render={({ state, fullpageApi }) => (
+                <ReactFullpage.Wrapper>
+                    <div className="section">
+                        <Link to="/page-2/">Go to page 2</Link>
+                        <button onClick={() => fullpageApi.moveSectionDown()}>
+                            Click me to move down
+                        </button>
+                    </div>
+                    <div className="section">
+                    </div>
+                    <div className="section">
+                    </div>
+                    <div className="section">
+                    </div>
+                </ReactFullpage.Wrapper>
+            )}
+        />
+    </Layout>
 )
 
 export default IndexPage
